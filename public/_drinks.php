b2ff907ac45d2b0c104e1917d74a08a3eb4d6e42
jhicks5 <joshua.hicks@students.plymouth.ac.uk>
2019-12-19 22:25:55 +0000

    Drinks now organised better into card system

--- public/_drinks.php
+++ public/_drinks.php
@@ -5,16 +5,26 @@ include_once '../src/model/repository.php';
 
 $tablename = 'Products-Drinks';
 
+?>
+<div class="container pt-5">
+<div class="card-deck">
+<?php
 if (isset($tablename)) {
     $db = new repository();
     $results = $db->getAll($tablename);
 
     foreach($results as $row){
         ?>
-        <div class = "card hovercard">
-            <div class="card-body">
-                <a href="#"><?php echo $row['Product_Details']; ?></a>
-            </div>
+        <div class="card" style="width:400px">
+            <a href="#" style="color:black">
+                <img class="card-img-top" src="../assets/img/drinkTN_temp.jpg" alt="Drink image">
+                <div class="card-body text-center">
+                    <p class="card-text"><?php echo $row['Product_Details']; ?></p>
+                    <p class="card-text">£<?php echo $row['Price']; ?></p>
+                </div>
+            </a>
         </div>
     <?php } ?>
 <?php } ?>
+</div>
+</div>
