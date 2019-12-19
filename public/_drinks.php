bc2dec81f434f5d76454cdf3481856611313a0af
jhicks5 <joshua.hicks@students.plymouth.ac.uk>
2019-12-19 22:32:27 +0000

    Food page now displays food items

--- public/_drinks.php
+++ public/_drinks.php
@@ -17,7 +17,7 @@ if (isset($tablename)) {
         ?>
         <div class="card" style="width:400px">
             <a href="#" style="color:black">
-                <img class="card-img-top" src="../assets/img/drinkTN_temp.jpg" alt="Drink image">
+                <img class="card-img-top" src="../assets/img/drinkTN_temp.png" alt="Drink image">
                 <div class="card-body text-center">
                     <p class="card-text"><?php echo $row['Product_Details']; ?></p>
                     <p class="card-text">£<?php echo $row['Price']; ?></p>
--- public/_food.php
+++ public/_food.php
@@ -1,2 +1,30 @@
 <?php
+
 include_once 'header.php';
+include_once '../src/model/repository.php';
+
+$tablename = 'Products-Food';
+
+?>
+<div class="container pt-5">
+    <div class="card-deck">
+        <?php
+        if (isset($tablename)) {
+            $db = new repository();
+            $results = $db->getAll($tablename);
+
+            foreach($results as $row){
+                ?>
+                <div class="card" style="width:400px">
+                    <a href="#" style="color:black">
+                        <img class="card-img-top" src="../assets/img/foodTN_temp.png" alt="Drink image">
+                        <div class="card-body text-center">
+                            <p class="card-text"><?php echo $row['Product_Details']; ?></p>
+                            <p class="card-text">£<?php echo $row['Price']; ?></p>
+                        </div>
+                    </a>
+                </div>
+            <?php } ?>
+        <?php } ?>
+    </div>
+</div>
