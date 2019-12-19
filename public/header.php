92da4aabae342709b0f5dc2e536a26e4c2f2a136
jhicks5 <joshua.hicks@students.plymouth.ac.uk>
2019-12-19 22:36:52 +0000

    Minor changes to href links

--- public/header.php
+++ public/header.php
@@ -56,7 +56,7 @@
             <a href="landing.php" class="w3-button w3-block w3-black">HOME</a>
         </div>
         <div class="w3-col s4">
-            <a href="#orders" class="w3-button w3-block w3-black">ORDERS</a>
+            <a href="_orders.php" class="w3-button w3-block w3-black">ORDERS</a>
         </div>
         <div class="w3-col s4">
             <a href="login.php" class="w3-button w3-block w3-black">LOGIN</a>
