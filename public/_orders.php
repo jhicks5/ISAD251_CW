8ed169f2d719ada3f18376567ac9d1583bb6d5b7
jhicks5 <joshua.hicks@students.plymouth.ac.uk>
2019-12-30 20:31:53 +0000

    Orders can be selected and the details shown.

--- public/_orders.php
+++ public/_orders.php
@@ -6,61 +6,26 @@ if(empty($_SESSION['currentCustID'])){
 }
 ?>
 
-<div class="w3-cell">
-    <?php
-    $tablename = 'orderdetails';
-    if(isset($tablename)) {
-        $db = new Repository();
-        $results = $db->getAll($tablename);
+<form action="../public/_ordersdetails.php" method="GET">
+    <div class="container pt-5">
+        <div class="form-group">
+            <label for="selectOrderForm">Select The Order ID</label>
+            <select class="form-control form-control-lg" id="selectcontrol" name="selectcontrol">
+            <?php
+            $tablename = 'CustOrders';
+            if (isset($tablename)) {
+                $db = new repository();
+                $results = $db->getAll($tablename);
 
-        if ($results) {
-            //Hopefully if the results have been the right PDO type we should be able
-            //to extract the column names with ease.
-            $columns = empty($results) ? array() : array_keys($results[0]);
-            $idColumn = $columns[0];
-            $tableString = '<table class="w3-table w3-striped w3-bordered"><tr>';
-            $inputString = '';
-            $insertString = '';
-            foreach ($columns as $column) {
-                $tableString .= '<th>' . $column . '</th>';
-                $inputString .= '<th>' . $column . '</th>';
-                $insertString .= '<td><input type=\'text\' name=\'' . $column . '\'/></td>';
 
-            }
-            echo $tableString;
-            foreach ($results as $row) {
-                echo '<tr>';
-
-                foreach ($row as $cell) {
-                    echo '<td>' . $cell . '</td>';
-                }
-            }
-            echo '</table>';
-        }
-    }
-    ?>
-</div>
-
-<div class="container pt-5">
-    <div class="card-deck mx-auto" style="width:600px">
-        <?php
-        $tablename = 'CustOrders';
-        if (isset($tablename)) {
-            $db = new repository();
-            $results = $db->getAll($tablename);
-
-            foreach($results as $row){
-                ?>
-                <div class="card">
-                    <a href="#" style="color:black">
-                        <img class="card-img-top" src="../assets/img/orders_btn.png" alt="Order image">
-                        <div class="card-body text-center">
-                            <p class="card-text">Order ID: <?php echo $row['OrderId']; ?></p>
-                            <p class="card-text">Order Date: <?php echo $row['OrderDate']; ?></p>
-                        </div>
-                    </a>
-                </div>
+                foreach($results as $row){
+                    ?>
+                    <option id="selected_order"><?php echo $row['OrderId']?></option>
+                <?php } ?>
             <?php } ?>
-        <?php } ?>
+            </select>
+        </div>
+        <button type="Submit" value="Submit" id="Submit" class="btn btn-primary">See Details</button>
     </div>
-</div>
+</form>
+
--- public/_ordersdetails.php
+++ public/_ordersdetails.php
@@ -1,4 +1,42 @@
 <?php
 include_once 'header.php';
+include_once '../src/model/repository.php';
 
+$selectedOrder = $_GET['selectcontrol'];
+?>
 
+<div class="container pt-5">
+    <?php
+    $tablename = 'OrderDetails';
+    $_SESSION["currentOrderID"] = $selectedOrder;
+    if(isset($tablename)) {
+        $db = new Repository();
+        $results = $db->getAll($tablename);
+
+        if ($results) {
+            //Hopefully if the results have been the right PDO type we should be able
+            //to extract the column names with ease.
+            $columns = empty($results) ? array() : array_keys($results[0]);
+            $idColumn = $columns[0];
+            $tableString = '<table class="w3-table w3-striped w3-bordered"><tr>';
+            $inputString = '';
+            $insertString = '';
+            foreach ($columns as $column) {
+                $tableString .= '<th>' . $column . '</th>';
+                $inputString .= '<th>' . $column . '</th>';
+                $insertString .= '<td><input type=\'text\' name=\'' . $column . '\'/></td>';
+
+            }
+            echo $tableString;
+            foreach ($results as $row) {
+                echo '<tr>';
+
+                foreach ($row as $cell) {
+                    echo '<td>' . $cell . '</td>';
+                }
+            }
+            echo '</table>';
+        }
+    }
+    ?>
+</div>
